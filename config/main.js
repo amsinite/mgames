8648c7ed4fc69c8cad03d5d4ac1e7d72c0219c2d
amsinite <williamseinfeldthelast@gmail.com>
2025-05-28 20:31:07 -0600

    Update main.js

--- config/main.js
+++ config/main.js
@@ -1,6 +1,6 @@
 // This changes the title of your site
 
-var sitename = "mgames"; // Change this to change the name of your website.
+var sitename = "MGames"; // Change this to change the name of your website.
 var subtext = "v1.52"; // set the subtext
 
 // more settings in main.css
