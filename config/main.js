cebf9ff60604465646959c97f625c5cd5413727c
amsinite <williamseinfeldthelast@gmail.com>
2025-05-29 13:29:45 -0600

    Update main.js

--- config/main.js
+++ config/main.js
@@ -12,7 +12,7 @@ var subtext = "v1.52"; // set the subtext
 
 import "/./config/custom.js";
 
-var serverUrl1 = "https://amsinite.github.io/mgameserver/";
+var serverUrl1 = "https://amsinite.github.io/gms/";
 var currentPageTitle = document.title;
 document.title = `${currentPageTitle} | ${sitename}`;
 let gamesData = []; 
